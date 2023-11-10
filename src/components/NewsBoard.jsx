99e52c5d9e7ed7e9534eead52986f0301aba03bb
kelvin <ebukakels@gmail.com>
2023-11-10 13:25:48 +0100

    news-mag

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -3,37 +3,23 @@ import NewsItem from "./NewsItem";
 
 const NewsBoard = ({ category }) => {
     const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         let URL = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=496a221f0afe4153863bd4542e970421`;
         fetch(URL)
-            .then(response => response.json())
-            .then(data => {
-                setArticles(data.articles);
-                setLoading(false);
-            })
+            .then(response => response.json()) // Corrected: Call .json() as a function
+            .then(data => setArticles(data.articles))
             .catch(error => {
                 console.error('Error fetching data:', error);
-                setError('An error occurred while fetching the news.');
-                setLoading(false);
+                // Handle the error, e.g., set an error state or display an error message.
             });
     }, [category]);
-
-    if (loading) {
-        return <p>Loading...</p>; // Add a loading indicator
-    }
-
-    if (error) {
-        return <p>{error}</p>; // Display an error message
-    }
 
     return (
         <div>
             <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-            {articles.map(news => (
-                <NewsItem key={news.title} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+            {articles.map((news, index) => (
+                <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
             ))}
         </div>
     );
