0a539449e017ca67e4eb53bb7a2f1d3a35c7452c
kelvin <ebukakels@gmail.com>
2023-11-10 14:28:36 +0100

    news

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,28 +1,52 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 
 const NewsBoard = ({ category }) => {
     const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        let URL = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=496a221f0afe4153863bd4542e970421`;
-        fetch(URL)
-            .then(response => response.json())
-            .then(data => setArticles(data.articles))
-            .catch(error => {
+        const fetchData = async () => {
+            setLoading(true);
+
+            try {
+                let URL = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=496a221f0afe4153863bd4542e970421`;
+                const response = await fetch(URL);
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                setArticles(data.articles);
+            } catch (error) {
                 console.error('Error fetching data:', error);
-                // Handle the error, e.g., set an error state or display an error message.
-            });
+                setError('An error occurred while fetching the news.');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
     }, [category]);
 
     return (
         <div>
             <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
-            {Array.isArray(articles) && articles.map((news, index) => (
-                <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
-            ))}
+
+            {loading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>{error}</p>
+            ) : (
+                Array.isArray(articles) &&
+                articles.map((news, index) => (
+                    <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+                ))
+            )}
         </div>
     );
-}
+};
 
 export default NewsBoard;
